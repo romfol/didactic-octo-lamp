50a6b7e0c3190dd00618d17700233b4660c7ce92
romfol <romanfoltyn1@gmail.com>
2019-04-15 13:20:20 +0300

    curent button on cuttent item check added

--- src/components/item-buttons/item-buttons.jsx
+++ src/components/item-buttons/item-buttons.jsx
@@ -2,29 +2,34 @@ import React, { Component } from 'react';
 import './item-buttons.css';
 
 export default class ItemButtons extends Component {
+  state = { activeButton: 'button1' };
+
   hanleClick = e => {
+    this.setState({ activeButton: e.target.id });
     console.log(e.target.id);
   };
   render() {
+    const { activeButton } = this.state;
+
     return (
-      <div className="item__buttons">
-        <div className="outer-circle">
-          <button className="button1" />
+      <div className="item__buttons" onClick={e => this.hanleClick(e)}>
+        <div className={activeButton === 'button1' ? 'outer-circle' : ''}>
+          <button id="button1" />
         </div>
-        <div>
-          <button className="button2" />
+        <div className={activeButton === 'button2' ? 'outer-circle' : ''}>
+          <button id="button2" />
         </div>
-        <div>
-          <button className="button3" />
+        <div className={activeButton === 'button3' ? 'outer-circle' : ''}>
+          <button id="button3" />
         </div>
-        <div>
-          <button onClick={e => this.hanleClick(e)} id="button4" />
+        <div className={activeButton === 'button4' ? 'outer-circle' : ''}>
+          <button id="button4" />
         </div>
-        <div>
-          <button className="button5" />
+        <div className={activeButton === 'button5' ? 'outer-circle' : ''}>
+          <button id="button5" />
         </div>
-        <div>
-          <button className="button6" />
+        <div className={activeButton === 'button6' ? 'outer-circle' : ''}>
+          <button id="button6" />
         </div>
       </div>
     );
