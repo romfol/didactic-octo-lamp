33bc58e27405a16db11c8dd9d3b4ba38ff29269d
romfol <romanfoltyn1@gmail.com>
2019-04-16 12:05:48 +0300

    refactored a bit

--- src/components/item-buttons/item-buttons.jsx
+++ src/components/item-buttons/item-buttons.jsx
@@ -5,16 +5,16 @@ import colors from '../helpers/colors';
 export default class ItemButtons extends Component {
   state = { activeButton: 0 };
 
-  hanleClick = i => {
-    this.setState({ activeButton: i });
-  };
   render() {
     const { activeButton } = this.state;
 
     const buttons = colors.map((color, i) => {
       return (
         <div className={activeButton === i ? 'outer-circle' : ''} key={i}>
-          <button onClick={() => this.hanleClick(i)} style={{ backgroundColor: color }} />
+          <button
+            onClick={() => this.setState({ activeButton: i })}
+            style={{ backgroundColor: color }}
+          />
         </div>
       );
     });
