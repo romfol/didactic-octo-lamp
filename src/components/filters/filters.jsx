b85f8594fc272fd13b5f87570f016670ab5e8ca1
romfol <romanfoltyn1@gmail.com>
2019-04-17 17:13:56 +0300

    filter buttons styled well

--- src/components/filters/filters.jsx
+++ src/components/filters/filters.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './filters.css';
-import { filterButtons } from '../helpers';
+import { filterButtons as buttons } from '../helpers';
 
 export const Filters = () => {
   return (
     <section className="filters">
-      {filterButtons.map((title, i) => {
+      {buttons.map((title, i) => {
         return <button key={i}>{title}</button>;
       })}
     </section>
