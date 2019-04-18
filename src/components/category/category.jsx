5123b119b20cfad9c5a5144687fa36d53ff272a0
romfol <romanfoltyn1@gmail.com>
2019-04-18 17:58:12 +0300

    pagination logic v2

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -42,7 +42,6 @@ export default class Category extends Component {
       )
       .then(response => {
         this.setState({
-          activePage: this.state.activePage - 1,
           data: response.data.result.data,
         });
       })
