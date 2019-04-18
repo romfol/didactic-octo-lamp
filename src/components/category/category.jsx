8c32fa3684512cb3e4464c7749f62e511c46504a
romfol <romanfoltyn1@gmail.com>
2019-04-18 11:30:59 +0300

    rugs loader added

--- src/components/category/category.jsx
+++ src/components/category/category.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import Item from '../item/item';
+import Pagination from '../pagination/pagination';
 import { ArrowDown } from '../icons';
+import {Loader} from '../loader/loader';
 import './category.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default class Category extends Component {
-  state = { data: [] };
+  state = { data: [], totalItems: <Loader /> };
 
   componentDidMount() {
     axios
-      .get('https://qa-api.wovenlyrugs.com/products?page=1&page_size=12&size=runners&group=Rug')
+      .get('https://qa-api.wovenlyrugs.com/products?page=25&page_size=12&size=runners&group=Rug')
       .then(response => {
-        this.setState({ data: response.data.result.data });
+        console.log(response);
+        this.setState({
+          data: response.data.result.data,
+          totalItems: response.data.result.total_count,
+        });
+        console.log(this.state.totalItems);
       })
       .catch(function(error) {
         console.log(error);
@@ -20,7 +27,7 @@ export default class Category extends Component {
   }
 
   render() {
-    const { data } = this.state;
+    const { data, totalItems } = this.state;
 
     const productList = data.map(item => (
       <li key={item.id} className="items__row">
@@ -31,12 +38,13 @@ export default class Category extends Component {
     return (
       <section className="category">
         <div className="category__data">
-          <span className="category__title">769 Rugs</span>
+          <span className="category__title">{totalItems} Rugs</span>
           <button className="category__sort">
             Sort <ArrowDown />
           </button>
         </div>
         <ul className="items">{productList}</ul>
+        <Pagination />
         <h1 className="category__greet">Hello category page</h1>
         <Link to="/">Go to Home page</Link>
       </section>
---
+++ src/components/pagination/pagination.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './pagination.css';
+
+export default class Pagination extends Component {
+  render() {
+    return <section className="pagination">Pagination</section>;
+  }
+}
