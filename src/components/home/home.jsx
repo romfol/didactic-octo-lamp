e0f83cbb39256ce02bde0913a26fc58b269b0cba
romfol <romanfoltyn1@gmail.com>
2019-04-25 10:49:57 +0300

    homepage slider that works

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from '../slider/slider';
+import axios from 'axios';
 import './home.css';
 
-const Home = () => {
-  return (
-    <section className="home">
-      <Slider />
-      <Link to="products">Go to Category</Link>
-    </section>
-  );
-};
+export default class Home extends Component {
+  state = {
+    data: [],
+  };
 
-export default Home;
+  componentDidMount() {
+    axios
+      .get(`https://qa-api.wovenlyrugs.com/products?page=1&page_size=20&size=runners&group=Rug`)
+      .then(response => {
+        this.setState({
+          data: response.data.result.data,
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <section className="home">
+        <Slider data={this.state.data} />
+        <br />
+        <Link to="products">Go to Category</Link>
+      </section>
+    );
+  }
+}
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
+import Item from '../item/item';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
-export default class Slider extends Component {
+export default class SimpleSlider extends Component {
   render() {
-    return <section className="slider">Slide me</section>;
+    console.log(this.props);
+    const settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+    };
+
+    return (
+      <section>
+        <Slider {...settings}>
+          {this.props.data.map(item => (
+            <Item className="item" {...item} />
+          ))}
+        </Slider>
+      </section>
+    );
   }
 }
