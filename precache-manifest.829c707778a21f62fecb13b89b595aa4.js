0c7ff34d5a12e281e8524144c5d52630f585c811
romfol <romanfoltyn1@gmail.com>
2019-05-22 20:27:10 +0300

    Updates

---
+++ precache-manifest.829c707778a21f62fecb13b89b595aa4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cfa44a7be0331ca3007",
+    "url": "/online-store/static/css/main.ff9c9a6b.chunk.css"
+  },
+  {
+    "revision": "4cfa44a7be0331ca3007",
+    "url": "/online-store/static/js/main.7d686645.chunk.js"
+  },
+  {
+    "revision": "19e0f7521b8bac23d01b",
+    "url": "/online-store/static/js/runtime~main.ca6cc33d.js"
+  },
+  {
+    "revision": "4bd52adba4fac547e78e",
+    "url": "/online-store/static/css/2.252dd9e4.chunk.css"
+  },
+  {
+    "revision": "4bd52adba4fac547e78e",
+    "url": "/online-store/static/js/2.90079ef3.chunk.js"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/online-store/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/online-store/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/online-store/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/online-store/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "ce127a9d884dfea18cd550c75fcf0ed9",
+    "url": "/online-store/static/media/background-header.ce127a9d.jpg"
+  },
+  {
+    "revision": "10fe25fa6ef89e5d097229ce23fa499b",
+    "url": "/online-store/static/media/Graphik-Medium-Web.10fe25fa.woff"
+  },
+  {
+    "revision": "1d85e7610acbe6b44cb4d00ae7842ee3",
+    "url": "/online-store/static/media/Graphik-Regular-Web.1d85e761.woff"
+  },
+  {
+    "revision": "a02f00d09524bc69dcec72046ebc55cf",
+    "url": "/online-store/static/media/Graphik-Semibold-Web.a02f00d0.woff"
+  },
+  {
+    "revision": "6876e4341ff22466fad6e30ad3a33f2b",
+    "url": "/online-store/index.html"
+  }
+];
